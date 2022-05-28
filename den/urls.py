b9919f35e71127e05ec1731cab210583cbef0b4a
Sammy Muchiri <dev.miano@gmail.com>
2022-05-28 03:09:45 +0300

    added the views and routes for the details page

--- den/urls.py
+++ den/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('<category>/', views.base, name='base'),
+  path('<category_name>/<int:cat_id>/', views.detail, name='detail'),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- den/views.py
+++ den/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render
 from .models import Cat, Category
 
-
-def base(request):
+def base(request, category):
   categories = Category.objects.order_by('-url')
+  template = 'den/base.html'
+  
   context = {
-    'categories': categories
+    'category': category,
+    'categories': categories,
   }
-  return render(request, 'den/base.html', context)
+  
+  return render(request, template, context)
 
 def index(request):
   categories = Category.objects.order_by('-url')
   latest_cats = Cat.objects.order_by('-posted')[:4]
   title = "Visit the Den of Africas' Big Cats"
+  template = 'den/index.html'
   
   context = {
     'title': title,
     'categories': categories,
     'latest_cats': latest_cats,
   }
-  return render(request, 'den/index.html', context)
+  
+  return render(request, template, context)
+
+def detail(request, category_name, cat_id ):
+  cat = Cat.objects.filter(id=cat_id).first()
+  categories = Category.objects.order_by('-url')
+  category = Category.objects.get(url=category_name)
+  title = f'{cat.title}'
+  template = 'den/detail.html'
+  
+  if category.id == cat.category:
+    category_name = category.url
+    
+  context = {
+    'cat': cat,
+    'title': title,
+    'categories': categories,
+    'category_name': category_name
+  }
+  
+  return render(request, template, context)
