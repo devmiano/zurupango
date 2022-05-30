777e8a7b069f832843dcc827aa4c00294d22c4bb
Sammy Muchiri <dev.miano@gmail.com>
2022-05-30 16:16:21 +0300

    added test for the location model

--- den/tests.py
+++ den/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Cat, Category, Location
 
-# Create your tests here.
+class LocationTestCase(TestCase):
+  def setUp(self):
+    self.mali = Location(url='mali', title='Mali')
+    
+  def test_instance(self):
+    self.assertTrue(isinstance(self.mali, Location))
+    
+# class CategoryTestCase(TestCase):
+#   def setUp(self):
+#     self
+
+# class CatTestCase(TestCase):
+#   def setUp(self):
+#     self
