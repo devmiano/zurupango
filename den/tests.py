947770e4100795878b66b1ce3006e0698e6a42de
Sammy Muchiri <dev.miano@gmail.com>
2022-05-30 16:20:15 +0300

    added test for the category model

--- den/tests.py
+++ den/tests.py
@@ -8,9 +8,12 @@ class LocationTestCase(TestCase):
   def test_instance(self):
     self.assertTrue(isinstance(self.mali, Location))
     
-# class CategoryTestCase(TestCase):
-#   def setUp(self):
-#     self
+class CategoryTestCase(TestCase):
+  def setUp(self):
+    self.tiger = Category(image='category/tiger.png', url='tigers', title='Tiger', genus='Panthera tigris', caption='New Tiger')
+    
+  def test_instance(self):
+    self.assertTrue(isinstance(self.tiger, Category))
 
 # class CatTestCase(TestCase):
 #   def setUp(self):
