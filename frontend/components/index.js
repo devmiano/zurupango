22282265b2fd44bb3dc103a430cd9fa4d84cb141
Sammy Muchiri <dev.miano@gmail.com>
2022-07-24 04:33:58 +0300

    added the latest cats to the hero section

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBrand } from "./NavBrand";
 export { default as Footer } from "./Footer";
 export { default as Sidebar } from "./Sidebar";
+export { default as CatCard } from "./CatCard";
